6431dc838df9dfb8439bfee2c2f08ca280b7288b
Zachary <zachjoliver@gmail.com>
2020-04-20 02:03:23 -0700

    Finish scales notes triads and random progression initial build

--- misc.js
+++ misc.js
@@ -1,10 +1,11 @@
 const notes = ['A','A#','B','C','C#','D','D#','E','F','F#','G','G#'];
 const majorPattern = 'WWHWWWH';
 const minorPattern = 'WHWWHWW';
-const majTriads = [4,3];
-const minTriads = [3,4];
+const majTriadPatt = [4,3];
+const minTriadsPatt = [3,4];
 
-const majorTriads = ['I','ii','iii','IV','V','vi'];
+const majorTriads = ['I','ii','iii','IV','V','vi', 'vii0'];
+const minorTriads = ['i','ii0','III','iv','VI','VII'];
 
 
 function scalesNotes(root, type) {
@@ -20,15 +21,42 @@ function scalesNotes(root, type) {
 }
 
 function scaleTriads(root,type = 'major') { //major only
-  const scale = (scalesNotes, type);
-
-  return scale.map(note => {
-    const semitones = (note === note.toUpperCase()) ? majTriads : minTriads;
+  const scale = scalesNotes( root, type);
+  const triads = type === 'major' ? majorTriads : minorTriads; 
+  
+  return triads.map((triad,i) => {
+    // const semitones = (triad === triad.toUpperCase()) ? majTriadPatt : minTriadsPatt;
+    return triad + ': ' + scale[i] + " " + scale[(i + 2) % scale.length] + " " + scale[(i + 4) % scale.length];
   })
 }
 
-const scale = scalesNotes('D#','minor');
-console.log(scale);
+function randomProgression(root, type, numChords, inclSeven) {
+  let triads = scaleTriads(root,type);
+  console.log(triads);
+  let prog = [];
+  let progNum = [];
+
+  for (let z=0;z<numChords;z++) {
+    let tris = inclSeven ? triads.length : triads.length - 1;
+    const rand = Math.floor(Math.random() * tris);
+    progNum.push(rand + 1);
+    prog.push(triads[rand]);
+  }
+  console.log(progNum);
+  return prog;
+}
+
+// const scale = scalesNotes('D#','minor');
+// console.log(scale);
+
+// const triads = scaleTriads('E#','major');
+// const triads = scaleTriads('E#','major',false);
+// console.log(triads);
+
+const progression = randomProgression('E','major',4);
+console.log(progression);
+
+
 
 //TO DO:
 //account for when a note can be called flat/sharp to make all notes appear in a scale
